bd2c288b883fcd04a988a7fb7df505eed23e836e
Magno Belloni <bellonidesouza6@gmail.com>
2026-02-11 11:45:59 -0300

    Removed unnused field in ViaCepResponse

--- src/F360.Domain/Dtos/Responses/External/ViaCepResponse.cs
+++ src/F360.Domain/Dtos/Responses/External/ViaCepResponse.cs
@@ -4,7 +4,6 @@ public class ViaCepResponse
 {
     public string Cep { get; set; } = string.Empty;
     public string Logradouro { get; set; } = string.Empty;
-    public string Complemento { get; set; } = string.Empty;
     public string Bairro { get; set; } = string.Empty;
     public string Localidade { get; set; } = string.Empty;
     public string Uf { get; set; } = string.Empty;
